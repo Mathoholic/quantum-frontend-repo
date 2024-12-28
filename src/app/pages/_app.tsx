604f31b1482124441f3908988fb59020a3050963
gaurav <gaurav@centocode.com>
2024-12-28 10:28:34 +0530

    minor changes

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -5,10 +5,11 @@ import Sidebar from './components/Sidebar';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <div style={{ display: 'flex', height: '100vh' }}>
-      {/* Sidebar */}
+   
       <Sidebar />
-      {/* Content Area */}
-      <div style={{ flexGrow: 1, marginLeft: '200px' }}> {/* Adjust the margin-left dynamically */}
+   
+      
+      <div style={{ flexGrow: 1, marginLeft: '200px' }}> 
         <Component {...pageProps} />
       </div>
     </div>
--- src/app/pages/components/Sidebar.tsx
+++ src/app/pages/components/Sidebar.tsx
@@ -1,23 +1,39 @@
+"use client"
+import { useState } from 'react';
 import Link from 'next/link';
 import styles from './Sidebar.module.css';
+import { FaBars, FaHome, FaUsers } from 'react-icons/fa'; // Importing the hamburger icon
 
 const Sidebar = () => {
-    const menuItems = [
-        { name: 'Lead View', path: '/pages/admin' },
-        { name: 'Members', path: '/pages/members' },
-    ];
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true); // State for toggling sidebar
 
-    return (
-        <div className={styles.sidebar}>
-            <ul>
-                {menuItems.map((item) => (
-                    <li key={item.name}>
-                        <Link href={item.path}>{item.name}</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const menuItems = [
+    { name: 'Lead View', path: '/pages/admin', icon: <FaHome /> },
+    { name: 'Members', path: '/pages/members', icon: <FaUsers /> },
+  ];
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen); 
+  };
+
+  return (
+    <div className={`${styles.sidebar} ${isSidebarOpen ? '' : styles.closed}`}>
+      <div className={styles.toggleButton} onClick={toggleSidebar}>
+        {/* <FaBars /> Hamburger icon */}
+      </div>
+
+      <ul>
+        {menuItems.map((item) => (
+          <li key={item.name}>
+            <Link href={item.path} className={styles.link}>
+              <span className={styles.icon}>{item.icon}</span>
+              {isSidebarOpen && item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
