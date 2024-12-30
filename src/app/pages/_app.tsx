282867260caed121dcaa66192b0c1f80d937c527
gaurav <gaurav@centocode.com>
2024-12-30 21:32:49 +0530

    added enquery form

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Sidebar from './components/Sidebar';
-
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-hot-toast';
+import { ToastContainer } from 'react-toastify';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <div style={{ display: 'flex', height: '100vh' }}>
@@ -11,6 +13,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       
       <div style={{ flexGrow: 1, marginLeft: '200px' }}> 
         <Component {...pageProps} />
+        <ToastContainer/>
       </div>
     </div>
   );
