0906ee7c8b5a13d01fbd4e3494096e682fa8a2c3
Paritosh Indalia <paritosh1332@gmail.com>
2025-01-03 11:59:05 +0530

    feat: conditionally render header and footer based on route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,23 @@
+'use client'
+
 import Header from '@/components/Header';
 import { ReactNode } from 'react';
 import './globals.css';
 import Footer from '@/components/Footer';
+import { usePathname } from 'next/navigation';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+  const noHeaderFooterRoutes = ['/admin','/form']; 
+
+  const showHeaderFooter = pathname !== null && !noHeaderFooterRoutes.includes(pathname);
+
   return (
     <html lang="en">
       <body>
-        <Header/>
+        {showHeaderFooter && <Header />}
         <main>{children}</main>
-        <Footer/>
+        {showHeaderFooter && <Footer />}
       </body>
     </html>
   );
