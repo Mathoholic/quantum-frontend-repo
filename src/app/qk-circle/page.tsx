24b1dd745b06518b4a792e6c16d2fec258c45aa3
Paritosh Indalia <paritosh1332@gmail.com>
2025-01-10 12:45:50 +0530

    Refactor layout by removing unused widgets, adjusting button styles, and improving responsiveness in FloatingButtons and ProgramHero components

--- src/app/qk-circle/page.tsx
+++ src/app/qk-circle/page.tsx
@@ -11,13 +11,7 @@ export default function Home() {
         <BlogSection />
         <div className="grid grid-row-1 md:grid-row-3 gap-4">
           <LightWidget />
-          <JuicerFeed/>
-          <div className="w-full relative bg-white shadow-lg rounded-lg px-8 py-4">
-            <div className="text-center font-semibold py-2">Facebook Widget Placeholder</div>
-          </div>
-          <div className="w-full relative bg-white shadow-lg rounded-lg px-8 py-4">
-            <div className="text-center font-semibold py-2">LinkedIn Widget Placeholder</div>
-          </div>
+          
         </div>
       </section>
     </div>
