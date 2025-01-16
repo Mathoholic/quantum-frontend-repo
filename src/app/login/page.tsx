6b32eaa3ef8d10bc4f5dc5ed7497fb2652cedb1a
Paritosh Indalia <paritosh1332@gmail.com>
2025-01-16 07:34:41 +0530

    Update API endpoints to use the production server IP address

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,7 +15,7 @@ const AdminLogin = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3002/admin/login', {
+      const response = await fetch('http://208.109.214.146:3002/admin/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
